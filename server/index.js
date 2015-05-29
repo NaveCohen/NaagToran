ebfe51d60ec3d8cab095c6aa61048d03a9bc847f
NaveCohen <nave.coheng@gmail.com>
2015-05-29 16:41:29 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,32 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\index.html");
+  res.sendFile("..\\na'agToran\\www\\index.html");
 });
 
 app.get('/ionicCss', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\lib\\ionic\\css\\ionic.css");
+  res.sendFile("..\\na'agToran\\www\\lib\\ionic\\css\\ionic.css");
 });
 app.get('/styleCss', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\css\\style.css");
+  res.sendFile("..\\na'agToran\\www\\css\\style.css");
 });
 app.get('/ionicBundleJs', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\lib\\ionic\\js\\ionic.bundle.js");
+  res.sendFile("..\\na'agToran\\www\\lib\\ionic\\js\\ionic.bundle.js");
 });
 app.get('/facebookPluginJs', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\js\\facebookPlugin.js");
+  res.sendFile("..\\na'agToran\\www\\js\\facebookPlugin.js");
 });
 app.get('/appJs', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\js\\app.js");
+  res.sendFile("..\\na'agToran\\www\\js\\app.js");
 });
 app.get('/facebookJs', function (req, res) {
-  res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\js\\facebook.js");
+  res.sendFile("..\\na'agToran\\www\\js\\facebook.js");
 });
 app.get('/mainImage', function (req, res) {
-	res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\images\\MainImage.jpg");
+	res.sendFile("..\\na'agToran\\www\\images\\MainImage.jpg");
 });
 app.get('/jquery', function (req, res) {
-	res.sendFile("D:\\Nave\\MyProjects\\Na'agToran\\na'agToran\\www\\js\\jquery-1.11.3.js");
+	res.sendFile("..\\na'agToran\\www\\js\\jquery-1.11.3.js");
 });
 app.get('/User/:email', function(req, res) {
 	// Check in the db if the user exists
