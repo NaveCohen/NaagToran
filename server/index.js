0f685e007935234b6c9475811a1f11f3ce6a703f
IdoSams <idoosams@gmail.com>
2015-05-29 17:04:11 +0300

    ChangeInIndexJS

--- server/index.js
+++ server/index.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var app = express();
+var dirName = "NaagToran";
 app.get('/', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\index.html");
+  res.sendFile("na'agToran\\www\\index.html", {root: dirName });
 });
 
 app.get('/ionicCss', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\lib\\ionic\\css\\ionic.css");
+  res.sendFile("na'agToran\\www\\lib\\ionic\\css\\ionic.css", {root: dirName });
 });
 app.get('/styleCss', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\css\\style.css");
+  res.sendFile("na'agToran\\www\\css\\style.css", {root: dirName });
 });
 app.get('/ionicBundleJs', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\lib\\ionic\\js\\ionic.bundle.js");
+  res.sendFile("na'agToran\\www\\lib\\ionic\\js\\ionic.bundle.js", {root: dirName });
 });
 app.get('/facebookPluginJs', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\js\\facebookPlugin.js");
+  res.sendFile("na'agToran\\www\\js\\facebookPlugin.js", {root: dirName });
 });
 app.get('/appJs', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\js\\app.js");
+  res.sendFile("na'agToran\\www\\js\\app.js", {root: dirName });
 });
 app.get('/facebookJs', function (req, res) {
-  res.sendFile("..\\na'agToran\\www\\js\\facebook.js");
+  res.sendFile("na'agToran\\www\\js\\facebook.js", {root: dirName });
 });
 app.get('/mainImage', function (req, res) {
-	res.sendFile("..\\na'agToran\\www\\images\\MainImage.jpg");
+	res.sendFile("na'agToran\\www\\images\\MainImage.jpg", {root: dirName });
 });
 app.get('/jquery', function (req, res) {
-	res.sendFile("..\\na'agToran\\www\\js\\jquery-1.11.3.js");
+	res.sendFile("na'agToran\\www\\js\\jquery-1.11.3.js", {root: dirName });
 });
 app.get('/User/:email', function(req, res) {
 	// Check in the db if the user exists
